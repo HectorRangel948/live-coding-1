0e305a86f7b82e9540dc0f9b63b2f19b5f65f25b
Hector Rangel <hector.rangel948@gmail.com>
2023-06-22 15:55:56 -0600

    Ejercicio de live coding 22/06/2023

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 // Escribe tu código aquí.
+
+var Arreglo = ["manzana", "pera", "mango", "fresa", "platano", "sandia", "melon", "durazno", "guayaba", "naranja"];
+var palabraEntrada = "lapiz";
+var respuesta =[];
+
+Arreglo.forEach((palabra, index) => {
+    if(palabra.length>palabraEntrada.length){
+        respuesta.push(palabra);
+    }
+    return respuesta;
+});
+
+var lista = document.getElementsByClassName(".lista");
+var dato = document.createElement("li");
+
+dato.appendChild(Arreglo[0]);
+lista.appendChild(dato);
+
+
+/*for(i=0; i<respuesta.length;i++){
+dato.document.appendChild(respuesta[n]);
+lista.document.appendChild(lista);
+}
+
+console.log(respuesta);*/
